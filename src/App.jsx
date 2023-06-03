ba7020d57073c04fbda4b73ff276ce88459128bd
Destiny Ezenwata <ezenwatairoegbu97@gmail.com>
2023-06-03 14:04:52 +0100

    custom button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react"
+import CustomButton from "./components/CustomButton"
 
 const App = () => {
 	return (
 		<div>
 			<h1>React App</h1>
+			<CustomButton text={"Talk to An expert"} />
 		</div>
 	)
 }
