9996d30c7ffb5ce9f9c8d91d0e2ff7c37e3ca8d9
Grace Ndimele <gracendimele5@gmail.com>
2023-06-03 20:10:06 +0100

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import CustomButton from "./components/CustomButton"
-import Navbar from "./components/Navbar"
+import Navbar from "./components/NavBar"
 
 const App = () => {
 	return (
 		<div>
-			<h1>React App</h1>
-			<CustomButton text={"Talk to An expert"} />
+			
 			<Navbar />
+			<CustomButton text={"Talk to An expert"} />
+			
 		</div>
 	)
 }
