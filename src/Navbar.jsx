d5611e92364e2da0d14a133cd07db0d4c13178ec
Grace Ndimele <gracendimele5@gmail.com>
2023-06-03 14:08:51 +0100

    nav

---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from './images/logo.png'
+const Navbar = () => {
+    return ( 
+        <div className="nav">
+            <img src={Logo} width="100px" className="logo"/>
+            
+        </div>
+     );
+}
+ 
+export default Navbar;
