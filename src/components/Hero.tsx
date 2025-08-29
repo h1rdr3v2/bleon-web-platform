d77a4ae1cb689d87f318424700fe988b9d9b5f97
Destiny Ezenwata <ezenwatairoegbu97@gmail.com>
2025-08-29 13:02:07 +0100

    removing unused var

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ArrowRight, Play } from 'lucide-react'
+import { ArrowRight } from 'lucide-react'
 import Link from 'next/link'
 import AnimatedWrapper from './AnimatedWrapper'
 
